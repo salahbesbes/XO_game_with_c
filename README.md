de9aa51c823f1bd400da575547a630ac72635b80
salahbesbes <salah_besbes@ymail.com>
2022-02-04 01:00:02 +0100

    readme

--- README.md
+++ README.md
@@ -21,15 +21,26 @@ Project is created with:
 
 ## Setup
 
-To run this project , u have to install SDL2, SDL2_image, SDL2_gfx
+- ## Linux
 
-    they are the same to install :
-    * dowload -> extact -> cd to folder
-    * ./configure
-    * make all
-    * sudo make install
-    * then in the make file add this to the flags u are compiling with :
-    	-L/usr/local/lib -lSDL2 -lm -lSDL2_image  -lSDL2_gfx
+  Make sure you have some thing to unzip files and GCC:
+
+        sudo apt-get update -y
+        sudo apt install unzip -y
+        sudo apt-get install -y libghc-sdl-gfx-dev
+        sudo apt install build-essential
+        sudo apt-get install xorg-dev
+        sudo apt-get install lib32z1
+
+  Install [SDL2](https://www.libsdl.org/release/SDL2-2.0.20.zip) & [SDL2_image](https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.zip) & [SDL_gfx](https://sourceforge.net/projects/sdl2gfx/)
+
+        unzip filename.zip -d ~
+        cd ~/SDL-folder
+        ./configure
+        make all
+        sudo make install
+
+- ## Windows
 
 ## to clone repo
 
@@ -42,3 +53,6 @@ To run this project , u have to install SDL2, SDL2_image, SDL2_gfx
     * execute:
     ./a.out
     * enjoy :)
+
+- then in the make file add this to the flags u are compiling with :
+  -L/usr/local/lib -lSDL2 -lm -lSDL2_image -lSDL2_gfx
--- renderGame.c
+++ renderGame.c
@@ -4,17 +4,17 @@
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <stdlib.h>
-const SDL_Color GRID_COLOR = { .r = 255, .g = 255, .b = 255 };
-const SDL_Color PLAYER_X_COLOR = { .r = 50, .g = 50, .b = 255 };
-const SDL_Color PLAYER_O_COLOR = { .r = 255, .g = 100, .b = 50 };
-const SDL_Color TIE_COLOR = { .r = 100, .g = 100, .b = 100 };
+const SDL_Color GRID_COLOR = {.r = 255, .g = 255, .b = 255};
+const SDL_Color PLAYER_X_COLOR = {.r = 50, .g = 50, .b = 255};
+const SDL_Color PLAYER_O_COLOR = {.r = 255, .g = 100, .b = 50};
+const SDL_Color TIE_COLOR = {.r = 100, .g = 100, .b = 100};
 /**
-* render_grid - render grid
-* @render: renderer
-* @color
-*
-* Return: void
-*/
+ * render_grid - render grid
+ * @render: renderer
+ * @color
+ *
+ * Return: void
+ */
 void render_grid(SDL_Renderer *render, const SDL_Color *color)
 {
 	int i = 1;
@@ -28,20 +28,18 @@ void render_grid(SDL_Renderer *render, const SDL_Color *color)
 	}
 }
 
-
-
 /**
-* render_x - render X
-* @render: renderer
-* @row: position in row
-* @column: position in column
-* @color: color
-* @arg: desc
-*
-* Return: void
-*/
+ * render_x - render X
+ * @render: renderer
+ * @row: position in row
+ * @column: position in column
+ * @color: color
+ * @arg: desc
+ *
+ * Return: void
+ */
 void render_x(SDL_Renderer *render, int row, int column,
-		const SDL_Color *color)
+	      const SDL_Color *color)
 {
 	const float half_box_side = fmin(CELLWIDTH, CELLHIGHT) * 0.25;
 	const float center_x = CELLWIDTH * 0.5 + column * CELLWIDTH;
@@ -50,43 +48,41 @@ void render_x(SDL_Renderer *render, int row, int column,
 	SDL_SetRenderDrawColor(render, color->r, color->g, color->b, color->a);
 
 	SDL_RenderDrawLine(render,
-			center_x - half_box_side,
-			center_y - half_box_side,
-			center_x + half_box_side,
-			center_y + half_box_side
-			);
+			   center_x - half_box_side,
+			   center_y - half_box_side,
+			   center_x + half_box_side,
+			   center_y + half_box_side);
 	SDL_RenderDrawLine(render,
-			center_x + half_box_side,
-			center_y - half_box_side,
-			center_x - half_box_side,
-			center_y + half_box_side
-			);
+			   center_x + half_box_side,
+			   center_y - half_box_side,
+			   center_x - half_box_side,
+			   center_y + half_box_side);
 	thickLineRGBA(render,
-			center_x - half_box_side,
-			center_y - half_box_side,
-			center_x + half_box_side,
-			center_y + half_box_side, 10,
-			color->r, color->g, color->b, 255);
+		      center_x - half_box_side,
+		      center_y - half_box_side,
+		      center_x + half_box_side,
+		      center_y + half_box_side, 10,
+		      color->r, color->g, color->b, 255);
 	thickLineRGBA(render,
-			center_x + half_box_side,
-			center_y - half_box_side,
-			center_x - half_box_side,
-			center_y + half_box_side, 10,
-			color->r, color->g, color->b, 255);
+		      center_x + half_box_side,
+		      center_y - half_box_side,
+		      center_x - half_box_side,
+		      center_y + half_box_side, 10,
+		      color->r, color->g, color->b, 255);
 	/*
-			*/
+	 */
 }
 
 /**
-* render_o - render  O
-* @render: renderer
-* @row: position in row
-* @column: position in column
-* @color: color
-* Return: void
-*/
+ * render_o - render  O
+ * @render: renderer
+ * @row: position in row
+ * @column: position in column
+ * @color: color
+ * Return: void
+ */
 void render_o(SDL_Renderer *render, int row, int column,
-		const SDL_Color *color)
+	      const SDL_Color *color)
 {
 
 	const float half_box_side = fmin(CELLWIDTH, CELLHIGHT) * 0.25;
@@ -95,33 +91,30 @@ void render_o(SDL_Renderer *render, int row, int column,
 
 	SDL_SetRenderDrawColor(render, color->r, color->g, color->b, color->a);
 	filledCircleRGBA(render,
-			center_x,
-			center_y,
-			half_box_side + 5,
-			color->r, color->g, color->b, 255
-			);
+			 center_x,
+			 center_y,
+			 half_box_side + 5,
+			 color->r, color->g, color->b, 255);
 	filledCircleRGBA(render,
-			center_x,
-			center_y,
-			half_box_side - 5,
-			0, 0, 0, 255
-			);
+			 center_x,
+			 center_y,
+			 half_box_side - 5,
+			 0, 0, 0, 255);
 }
 
 /**
-* render_board - render the content of each cell
-* @render: renderer
-* @board: array of int
-* @color_O: blue
-* @color_X: red
-*
-* Return: void
-*/
+ * render_board - render the content of each cell
+ * @render: renderer
+ * @board: array of int
+ * @color_O: blue
+ * @color_X: red
+ *
+ * Return: void
+ */
 void render_board(SDL_Renderer *render, int *board,
-		const SDL_Color *color_O, const SDL_Color *color_X)
+		  const SDL_Color *color_O, const SDL_Color *color_X)
 {
 	int i = 0, j = 0;
-
 
 	for (i = 0; i < N; i++)
 	{
@@ -129,26 +122,26 @@ void render_board(SDL_Renderer *render, int *board,
 		{
 			switch (board[i * N + j])
 			{
-				case Player_X:
-					render_x(render, i, j, color_X);
-					break;
-				case Player_O:
-					render_o(render, i, j, color_O);
-					break;
-				default:
-					break;
+			case Player_X:
+				render_x(render, i, j, color_X);
+				break;
+			case Player_O:
+				render_o(render, i, j, color_O);
+				break;
+			default:
+				break;
 			}
 		}
 	}
 }
 
 /**
-* render_running_state - render game when its not finiched yet
-* @render: renderer
-* @game: pointer to strcut game
-*
-* Return: void
-*/
+ * render_running_state - render game when its not finiched yet
+ * @render: renderer
+ * @game: pointer to strcut game
+ *
+ * Return: void
+ */
 void render_running_state(SDL_Renderer *render, game_t *game)
 {
 
@@ -156,46 +149,45 @@ void render_running_state(SDL_Renderer *render, game_t *game)
 	render_board(render, game->board, &PLAYER_O_COLOR, &PLAYER_X_COLOR);
 }
 /**
-* render_game_over_state - render game when one of the player won or in case of
-* tie
-* @render: renderer
-* @game: pointer to strcut game
-*
-* Return: void
-*/
+ * render_game_over_state - render game when one of the player won or in case of
+ * tie
+ * @render: renderer
+ * @game: pointer to strcut game
+ *
+ * Return: void
+ */
 void render_game_over_state(SDL_Renderer *render, game_t *game,
-		const SDL_Color *color)
+			    const SDL_Color *color)
 {
 	render_grid(render, color);
 	render_board(render, game->board, color, color);
 }
 
 /**
-* render_game - render game is different senario of the state
-* @arg: desc
-* @render: renderer
-* @game: pointer to strcut game
-*
-* Return: void
-*/
+ * render_game - render game is different senario of the state
+ * @arg: desc
+ * @render: renderer
+ * @game: pointer to strcut game
+ *
+ * Return: void
+ */
 void render_game(SDL_Renderer *render, game_t *game)
 {
 	switch (game->state)
 	{
-		case ISRunning:
-			render_running_state(render, game);
-			break;
-		case Pl_X_win:
-			render_game_over_state(render, game, &PLAYER_X_COLOR);
-			break;
-		case Pl_O_win:
-			render_game_over_state(render, game, &PLAYER_O_COLOR);
-			break;
-		case Tie:
-			render_game_over_state(render, game, &TIE_COLOR);
-			break;
-		default:
-			break;
+	case ISRunning:
+		render_running_state(render, game);
+		break;
+	case Pl_X_win:
+		render_game_over_state(render, game, &PLAYER_X_COLOR);
+		break;
+	case Pl_O_win:
+		render_game_over_state(render, game, &PLAYER_O_COLOR);
+		break;
+	case Tie:
+		render_game_over_state(render, game, &TIE_COLOR);
+		break;
+	default:
+		break;
 	}
-
 }
